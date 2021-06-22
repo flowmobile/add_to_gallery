7bac426e04b052213c0ddd6d477675f9332b857a
David King <david@oodavid.com>
2021-06-22 10:57:02 +0100

    Android > Unused imports

--- android/src/main/kotlin/flowmobile/add_to_gallery/AddToGalleryPlugin.kt
+++ android/src/main/kotlin/flowmobile/add_to_gallery/AddToGalleryPlugin.kt
@@ -1,9 +1,7 @@
 package flowmobile.add_to_gallery
 
-import android.app.Activity
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
